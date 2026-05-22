f62df20669ac7f3a910177db8d138eb8e35c2bbf
Haruki Martell <martellharuki@gmail.com>
2026-05-21 20:48:08 -0700

    basic unit tests

--- code-review-management/app/(pages)/(protected)/[username]/[repo_name]/pull/[id]/conflict-resolution/_components/ConflictResolution/useIsDark.ts
+++ code-review-management/app/(pages)/(protected)/[username]/[repo_name]/pull/[id]/conflict-resolution/_components/ConflictResolution/useIsDark.ts
@@ -1,33 +1,34 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 /**
  * Hook to get state of system setting for light/dark theme.
+ * We comment it out since we use light for now, but the infrastructure for dark mode exists
  */
 export default function useIsDark() {
-  const [isDark, setIsDark] = useState(false);
+  // const [isDark, setIsDark] = useState(false);
 
-  function refreshIsDark(event: MediaQueryListEvent) {
-    if (event.matches) {
-      setIsDark(true);
-    } else {
-      setIsDark(false);
-    }
-  }
+  // function refreshIsDark(event: MediaQueryListEvent) {
+  //   if (event.matches) {
+  //     setIsDark(true);
+  //   } else {
+  //     setIsDark(false);
+  //   }
+  // }
 
-  useEffect(() => {
-    setIsDark(window.matchMedia("(prefers-color-scheme: dark)").matches);
+  // useEffect(() => {
+  //   setIsDark(window.matchMedia("(prefers-color-scheme: dark)").matches);
 
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", refreshIsDark);
+  //   window
+  //     .matchMedia("(prefers-color-scheme: dark)")
+  //     .addEventListener("change", refreshIsDark);
 
-    return () => {
-      window
-        .matchMedia("(prefers-color-scheme: dark)")
-        .removeEventListener("change", refreshIsDark);
-    };
-  }, []);
+  //   return () => {
+  //     window
+  //       .matchMedia("(prefers-color-scheme: dark)")
+  //       .removeEventListener("change", refreshIsDark);
+  //   };
+  // }, []);
 
-  // We default to true because nothing else has dark mode
+  // // We default to true because nothing else has dark mode
   return { isDark: false };
 }
